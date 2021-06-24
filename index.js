8b70f68816c1a3ba6b564324cc98b89d03195b49
Karthikkevin <karthikkevin83@gmail.com>
2021-06-24 23:30:41 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,5 +2,32 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+//Creating Class and Executing the class by Creating Instance.
+class Class_A {
+  name = 'wert';
+  constructor(age) {
+    this.age = age;
+  }
+  greet() {
+    console.log(`Your name is ${this.name} and age is ${this.age}`);
+  }
+}
+const obj_A = new Class_A(4);
+obj_A.greet();
+
+//Constructor Function
+
+function ConstructorFunction_A(age) {
+  this.name = 'wert';
+  this.age = age;
+  this.greet = function() {
+    console.log(
+      `Constructor Function, Your name is ${this.name} and age is ${this.age}`
+    );
+
+    console.log(this);
+  };
+}
+const Conobj_A = new ConstructorFunction_A(4);
+Conobj_A.greet();
