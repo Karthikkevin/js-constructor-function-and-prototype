16c45adb1a78dc41293eb1b86257e4d8d207750a
Karthikkevin <karthikkevin83@gmail.com>
2021-06-25 08:45:07 +0530

    1 file modified

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const obj_A = new Class_A(4);
 obj_A.greet();
 
 //Constructor Function
-
 function ConstructorFunction_A(age) {
   this.name = 'wert';
   this.age = age;
@@ -31,3 +30,19 @@ function ConstructorFunction_A(age) {
 }
 const Conobj_A = new ConstructorFunction_A(4);
 Conobj_A.greet();
+
+//Constructor Function Vs Classes & Understanding "new"
+function funConstructor() {
+  // this={} -> While Creating the Instance for Class or Function Constructor at the time this will get created for that.
+
+  this.age = 20;
+  this.name = 'xyz';
+  this.fun = () => {
+    console.log('Welcome');
+  };
+  //return this; -> After Executing all the codes it will returns this
+}
+
+const fun = new funConstructor();
+fun.fun();
+console.log(fun.age);
